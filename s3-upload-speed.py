ae0bcc2c6dfcd4cd305479ff55ca55967f971992
Loren Charnley <loren@resonantcircuitlabs.com>
2020-09-29 06:17:15 -0400

    Added iterated file generation, started on S3 Uploads

--- s3-upload-speed.py
+++ s3-upload-speed.py
@@ -1,7 +1,12 @@
 import os
+import sys
 import time
+import boto3
 import click
 import logging
+import threading
+
+from botocore.exceptions import ClientError
 
 units_dict = {
     'k':1024,
@@ -21,7 +26,7 @@ prevent any caching from impacting the metrics. There needs to be enough \
 disk space for the files, and beware that large numbers of large files will \
 take time to generate')
 
-@click.option('--bucket', default='MyBucket', help='Name of Amazon S3 Bucket \
+@click.option('--bucket', default='p2vptpm-lcharnley-cenpro-12736', help='Name of Amazon S3 Bucket \
 name for upload test')
 
 @click.option('--loglevel', default='DEBUG', help='DEBUG, INFO, WARNING, ERROR, CRITICAL')
@@ -36,12 +41,32 @@ def run_test(size, iter, bucket, loglevel):
     logging.debug("Iterations parameter: %s", str(iter))
     logging.debug('S3 Bucket: %s', bucket)
     
-
-
     #Generate Files
-    generate_big_random_bin_file('test', file_size)
+    print("Generating " + str(iter) + " Test Files")
+    x = range(0, iter)
+    for n in x:
+        new_file_name = 'test_' + str(n)
+        generate_big_random_bin_file(new_file_name, file_size)
+
+    #Test Transfers
+    for n in x:
+        #Standard S3
+            #Timed Transfer
 
 
+            #Delete file from Bucket
+
+        #Acclerated S3
+            #Timed Transfer
+
+            #Delete file from Bucket
+
+
+    #Delete Files
+    for n in x:
+        new_file_name = 'test_' + str(n)
+        print("Removing " + new_file_name)
+        os.remove(new_file_name)
 
     logging.debug("End Time: %s", time.perf_counter())
 
@@ -74,6 +99,24 @@ def generate_big_random_bin_file(filename,size):
     pass
 
 
+class ProgressPercentage(object):
+
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self, bytes_amount):
+        # To simplify, assume this is hooked up to a single filename
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
 
 
 
